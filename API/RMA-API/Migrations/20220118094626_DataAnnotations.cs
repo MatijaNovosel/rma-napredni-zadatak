26b6cb1504d97da3b76c22b4a8f8e3821aab8b92
Matija Novosel <mnovosel5@gmail.com>
2022-01-18 10:46:39 +0100

    Data annotations

---
+++ API/RMA-API/Migrations/20220118094626_DataAnnotations.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMA_API.Migrations
+{
+    public partial class DataAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TodoItems",
+                keyColumn: "Description",
+                keyValue: null,
+                column: "Description",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TodoItems",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TodoItems",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- API/RMA-API/Models/Todo.cs
+++ API/RMA-API/Models/Todo.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RMA_API.Models
 {
   public class TodoItem
   {
     public int Id { get; set; }
+    [Required]
+    [MaxLength(255)]
     public string? Description { get; set; }
     public DateTime CreatedAt { get; set; }
   }
